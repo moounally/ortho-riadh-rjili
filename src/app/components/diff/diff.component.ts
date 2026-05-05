b3c109f99e8538dd71866a4769fd20b50d86d62b
Mohamed Dhia OUNALLY <Mohamed.OUNALLY@etu.uca.fr>
2026-05-05 11:34:28 -0400

    Fix Galerie Mobile : Implémentation d'un slider personnalisé tactile avec icône de dent dorée

--- src/app/components/diff/diff.component.ts
+++ src/app/components/diff/diff.component.ts
@@ -1,16 +1,27 @@
-import { Component, Input } from "@angular/core";
-import { NgClass } from "../../../../node_modules/@angular/common/common_module.d-NEF7UaHr";
+import { Component, Input, OnInit, ElementRef, HostListener } from "@angular/core";
+import { FormsModule } from '@angular/forms';
 
 @Component({
-    selector : 'diff-component',
-    standalone : true,
-    templateUrl : "./diff.component.html",
+    selector: 'diff-component',
+    standalone: true,
+    templateUrl: "./diff.component.html",
+    imports: [FormsModule]
 })
+export class DiffComponent implements OnInit {
+    @Input() left!: string;
+    @Input() right!: string;
 
-export class DiffComponent {
-    @Input()
-    left!: string;
-    
-    @Input()
-    right!: string
+    sliderValue: number = 50;
+    containerWidth: number = 0;
+
+    constructor(private el: ElementRef) {}
+
+    ngOnInit() {
+        this.onResize();
+    }
+
+    @HostListener('window:resize')
+    onResize() {
+        this.containerWidth = this.el.nativeElement.offsetWidth;
+    }
 }
